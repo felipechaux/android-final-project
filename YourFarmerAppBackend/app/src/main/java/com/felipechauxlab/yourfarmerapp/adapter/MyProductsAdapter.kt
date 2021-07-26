19ea7b1654a9404213820427708012a43f58aa8e
abel felipe chaux gutierrez <felipechauxx@misena.edu.co>
2021-07-25 21:57:14 -0500

    added loading image

--- YourFarmerAppBackend/app/src/main/java/com/felipechauxlab/yourfarmerapp/adapter/MyProductsAdapter.kt
+++ YourFarmerAppBackend/app/src/main/java/com/felipechauxlab/yourfarmerapp/adapter/MyProductsAdapter.kt
@@ -4,10 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.yourfarmerapp.R
 import com.felipechauxlab.yourfarmerapp.restApi.dto.PublishProductDTO
+import com.squareup.picasso.Picasso
 
 class MyProductsAdapter(private val listProducts: List<PublishProductDTO?>?, private val listener: (Int, PublishProductDTO?) -> Unit) : RecyclerView.Adapter<MyProductsAdapter.MyProductsAdapterViewHolder>() {
 
@@ -25,14 +27,15 @@ class MyProductsAdapter(private val listProducts: List<PublishProductDTO?>?, pri
             position: Int
     ) {
         val item: PublishProductDTO? = listProducts?.get(position)
+        getImageUrl(myProductsAdapterViewHolder.imageProduct,item?.productPhoto.toString())
         myProductsAdapterViewHolder.textProductName.text = item?.productName
         myProductsAdapterViewHolder.textProductQuantity.text = item?.productQuantity.toString()
 
         myProductsAdapterViewHolder.buttonEdit.setOnClickListener {
-            listener(it.id,item)
+            listener(it.id, item)
         }
         myProductsAdapterViewHolder.buttonDelete.setOnClickListener {
-            listener(it.id,item)
+            listener(it.id, item)
         }
     }
 
@@ -46,5 +49,14 @@ class MyProductsAdapter(private val listProducts: List<PublishProductDTO?>?, pri
         val textProductQuantity: TextView = itemView.findViewById(R.id.text_quantity)
         val buttonEdit: ImageButton = itemView.findViewById(R.id.btn_edit)
         val buttonDelete: ImageButton = itemView.findViewById(R.id.btn_delete)
+        val imageProduct: ImageView = itemView.findViewById(R.id.img_product)
+    }
+
+    private fun getImageUrl(image: ImageView, url: String) {
+        try {
+            Picasso.get().load(url).into(image)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
